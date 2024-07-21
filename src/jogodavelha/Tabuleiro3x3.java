de10e9074c0c1f284013f6bbb1590e6e380d0465
Guilherme Lerner <139267321+guilhermelerner@users.noreply.github.com>
2024-07-21 17:12:31 -0300

    Tabuleiro3x3.java

--- src/jogodavelha/Tabuleiro3x3.java
+++ src/jogodavelha/Tabuleiro3x3.java
@@ -1,5 +1,9 @@
 package jogodavelha;
 
+/**
+ * @author Guilherme Lerner e João Samuel Luy
+ * @version 1.2
+ */
 public class Tabuleiro3x3 extends Tabuleiro {
     public Tabuleiro3x3() {
         super(3);

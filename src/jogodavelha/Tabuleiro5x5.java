daf0c99d3418bae480d71434ad4863fe78ef7d18
Guilherme Lerner <139267321+guilhermelerner@users.noreply.github.com>
2024-07-21 17:12:44 -0300

    Tabuleiro5x5.java

--- src/jogodavelha/Tabuleiro5x5.java
+++ src/jogodavelha/Tabuleiro5x5.java
@@ -1,5 +1,8 @@
 package jogodavelha;
-
+/**
+ * @author Guilherme Lerner e João Samuel Luy
+ * @version 1.2
+ */
 public class Tabuleiro5x5 extends Tabuleiro {
     public Tabuleiro5x5() {
         super(5);

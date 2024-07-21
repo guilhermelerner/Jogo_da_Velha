eb0920c2e8b13e161c903e985ca0d0c2eee7f70c
Guilherme Lerner <139267321+guilhermelerner@users.noreply.github.com>
2024-07-21 17:11:18 -0300

    JogadorEnum.java

--- src/jogodavelha/JogadorEnum.java
+++ src/jogodavelha/JogadorEnum.java
@@ -1,15 +1,30 @@
 package jogodavelha;
 
+/**
+ * Enumeração que representa os dois jogadores no jogo da velha e seus respectivos símbolos.
+ *
+ * @author Guilherme Lerner e João Samuel Luy
+ * @version 1.2
+ */
 public enum JogadorEnum {
     JOGADOR1('X'),
     JOGADOR2('O');
 
     private final char simbolo;
 
+    /**
+     * Constrói uma nova enumeração de jogador com o símbolo especificado.
+     * @param simbolo O símbolo associado ao jogador, representado por um caractere.
+     */
     JogadorEnum(char simbolo) {
         this.simbolo = simbolo;
     }
 
+    /**
+     * Retorna o símbolo associado ao jogador.
+     *
+     * @return O símbolo do jogador, representado por um caractere.
+     */
     public char getSimbolo() {
         return simbolo;
     }

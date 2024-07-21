894d14f9be23badd90e5a124ce91ac49a1e0b632
Guilherme Lerner <139267321+guilhermelerner@users.noreply.github.com>
2024-07-21 17:08:16 -0300

    GerenciaJogadores.java

--- src/armazenamento/GerenciaJogadores.java
+++ src/armazenamento/GerenciaJogadores.java
@@ -1,6 +1,10 @@
 package armazenamento;
-
+/**
+ * @author Guilherme Lerner e João Samuel Luy
+ * @version 1.2
+ */
 public interface GerenciaJogadores {
+
     void salvarJogador(String var1, int var2);
 
     int recuperarPontuacao(String var1);

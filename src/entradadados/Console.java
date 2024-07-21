f49e19d854348d282e55bd38f60872eaa71cc3b2
Guilherme Lerner <139267321+guilhermelerner@users.noreply.github.com>
2024-07-21 17:09:35 -0300

    Console.java

--- src/entradadados/Console.java
+++ src/entradadados/Console.java
@@ -1,13 +1,24 @@
 package entradadados;
+
 import excecoes.NomeJogadorInvalidoException;
 import excecoes.NomeJogadorRepetidoException;
 import jogodavelha.Jogada;
 import jogodavelha.Jogador;
 import java.util.Scanner;
 
+/**
+ * @author Guilherme Lerner e João Samuel Luy
+ * @version 1.2
+ */
 public class Console {
     private static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * @param jogador O jogador que está fazendo a jogada.
+     * @param tamanhoTabuleiro O tamanho do tabuleiro, usado para validar a entrada do jogador.
+     * @return Um objeto {@link Jogada} que representa a linha, coluna e o jogador que fez a jogada.
+     * @exception IllegalArgumentException Se a entrada do jogador estiver fora dos limites do tabuleiro.
+     */
     public static Jogada lerJogada(Jogador jogador, int tamanhoTabuleiro) {
         int linha, coluna;
         while (true) {
